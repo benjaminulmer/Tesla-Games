e64ef8825819b83cc2fe50bde1dd4ad7da2a41c3
YZ-YI <yiyuzhe0125@outlook.com>
2020-02-20 13:11:41 -0700

    turned on depth testing

--- TeslaGamesEngine/TeslaGamesEngine.cpp
+++ TeslaGamesEngine/TeslaGamesEngine.cpp
@@ -520,6 +520,9 @@ int main()
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
+		//turn on depth testing
+		glEnable(GL_DEPTH_TEST);
+
 		// Setup shader
 		shaderList[0].UseShader();
 		uniformModel = shaderList[0].GetModelLocation();
